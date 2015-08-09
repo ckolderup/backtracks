2770d63466b593510e7bfd56430f3fb9be7d3ab3
Casey Kolderup <casey@kolderup.org>
2015-08-08 19:51:54 -0500

    rubocop ;)

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -39,6 +39,7 @@ class UsersController < ApplicationController
   private
 
   def user_params
-    params.require(:user).permit(:id, :email, :display_name, :password, :password_confirmation, :send_weekly_email)
+    params.require(:user).permit(:id, :email, :display_name,
+      :password, :password_confirmation, :send_weekly_email)
   end
 end
