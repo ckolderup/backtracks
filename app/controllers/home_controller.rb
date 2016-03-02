14cc3a1cd05e6d260cecb832709054b997caff14
Casey Kolderup <casey@kolderup.org>
2016-03-02 17:41:30 -0500

    change latest page to allow anyone to see it if the user has made it public

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -4,10 +4,27 @@ class HomeController < ApplicationController
   end
 
   def latest
-    if current_user.last_email_contents.blank? || params[:force]
-      html = Email::Compiler.chart_v1(current_user.lastfm_username)[:chart]
-      current_user.update(last_email_contents: html)
+    if params[:slug].nil?
+      if current_user
+        redirect_to latest_url(current_user.slug) and return
+      else
+        redirect_to(not_found_path) and return
+      end
     end
-    @email = current_user.last_email_contents.html_safe
+
+    this_user = User.where(slug: params[:slug]).first
+
+    if this_user.present? && (this_user.public_chart? || current_user == this_user)
+      if this_user.last_email_contents.blank? || params[:force]
+        html = Email::Compiler.chart_v1(current_user.lastfm_username)[:chart]
+        current_user.update(last_email_contents: html)
+      end
+
+      @email = this_user.last_email_contents.html_safe
+      @lastfm_user = this_user.lastfm_username
+      render and return
+    else
+      redirect_to(not_found_path) and return
+    end
   end
 end
